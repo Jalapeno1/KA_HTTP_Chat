628ac172f19c35d54e6f407e6f5bf8eab3b64c40
Englund92 <S_englund0110@hotmail.com>
2014-09-09 14:09:24 +0200

    main added to ChatClient

--- KA_HTTP_Chat/src/ka_http_chat/ChatClient.java
+++ KA_HTTP_Chat/src/ka_http_chat/ChatClient.java
@@ -1,6 +1,8 @@
 package ka_http_chat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -8,6 +10,7 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
 
 public class ChatClient extends Thread {
 
@@ -45,5 +48,64 @@ public class ChatClient extends Thread {
         output.println(msg);
     }
 
+    public void stopClient() throws IOException {
+        output.println(ProtocolStrings.STOP);
+    }
+
+    public void run() {
+        String msg
+                = input.nextLine();
+        while (!msg.equals(ProtocolStrings.STOP)) {
+            notifyListeners(msg);
+            msg = input.nextLine();
+        }
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            java.util.logging.Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE,
+                    null,
+                    ex);
+        }
+
+    }
+//        public static void main(String[] args)
+//    {
+//        int port = 9090;
+//        String ip = "localhost";
+//        if (args.length == 2)
+//        {
+//            port = Integer.parseInt(args[0]);
+//            ip = args[1];
+//        }
+//        try
+//        { 
+//            DummyListener dummy = new DummyListener();
+//            EchoClient tester = new EchoClient();
+//            tester.registerEchoListener(dummy);
+//            tester.connect(ip, port);       
+//            System.out.println("Sending 'Hello world'");
+//            tester.send("Hello World");
+//            System.out.println("Waiting for a reply");     
+//            tester.stopClient();
+//            System.in.read();      
+//        } catch (UnknownHostException ex)
+//        {
+//            java.util.logging.Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex)
+//        {
+//            java.util.logging.Logger.getLogger(ChatClient.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        
+//    }
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 4711);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println("Hello World");
+        String result = in.readLine();
+        System.out.println("Result was: " + result);
+
+    }
 
 }
