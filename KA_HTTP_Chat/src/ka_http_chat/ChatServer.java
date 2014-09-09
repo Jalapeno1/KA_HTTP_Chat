d8f49f3ace71a0fa9b598156a1f26aea755e47fb
Jalapeno1 <jons_91@hotmail.com>
2014-09-09 13:29:34 +0200

    ProtocolStrings class created
    ChatServer in progress

--- KA_HTTP_Chat/src/ka_http_chat/ChatServer.java
+++ KA_HTTP_Chat/src/ka_http_chat/ChatServer.java
@@ -6,8 +6,14 @@
 
 package ka_http_chat;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.logging.Level;
 
 /**
  *
@@ -19,6 +25,48 @@ public class ChatServer
     private static ServerSocket serverSocket;
     private static final Properties properties = Logger.initProperties("server.properties");
 
+    private static void handleClient(Socket socket) throws IOException
+    {
+        Scanner input = new Scanner(socket.getInputStream());
+        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+        String message = input.nextLine(); //IMPORTANT blocking call
+        java.util.logging.Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, String.format("Received the message: %1$S ", message));
+        while (!message.equals(ProtocolStrings.STOP))
+        {
+            writer.println(message.toUpperCase());
+            java.util.logging.Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, String.format("Received the message: %1$S ", message.toUpperCase()));
+            message = input.nextLine(); //IMPORTANT blocking call
+        }
+        writer.println(ProtocolStrings.STOP);//Echo the stop message back to the client for a nice closedown
+        socket.close();
+        java.util.logging.Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, "Closed a Connection");
+    }
     
-    
+    public static void main(String[] args)
+    {
+
+        int port = Integer.parseInt(properties.getProperty("port"));
+        String ip = properties.getProperty("serverIp");
+        String logFile = properties.getProperty("logFile");
+
+        Logger.setLogFile(logFile, ChatServer.class.getName());
+        Logger.closeLogger(ChatServer.class.getName());
+
+        java.util.logging.Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, "Sever started");
+        try
+        {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(ip, port));
+            do
+            {
+                Socket socket = serverSocket.accept(); //Important Blocking call
+                java.util.logging.Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, "Connected to a client");
+                handleClient(socket);
+            } while (keepRunning);
+        } catch (IOException ex)
+        {
+            java.util.logging.Logger.getLogger(ChatServer.class.getName()).log(Level.INFO, "Connected to	a client");
+        }
+    }
 }
---
+++ KA_HTTP_Chat/src/ka_http_chat/ProtocolStrings.java
@@ -0,0 +1,16 @@
+package ka_http_chat;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jonas
+ */
+public class ProtocolStrings
+{
+    public static final String STOP = "##STOP##";
+}
