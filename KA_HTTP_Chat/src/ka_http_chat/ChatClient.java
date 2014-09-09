ea41c7a24f8c6d4159af6f138f15a259ec893a63
Englund92 <S_englund0110@hotmail.com>
2014-09-09 13:44:50 +0200

    Added code to Chatclient

--- KA_HTTP_Chat/src/ka_http_chat/ChatClient.java
+++ KA_HTTP_Chat/src/ka_http_chat/ChatClient.java
@@ -1,16 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ka_http_chat;
 
-/**
- *
- * @author Jonas
- */
-public class ChatClient
-{
-    
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ChatClient extends Thread {
+
+    Socket socket;
+    private int port;
+    private InetAddress serverAddress;
+    private Scanner input;
+    private PrintWriter output;
+    List<ChatListener> listeners = new ArrayList<>();
+
+    public void registerEchoListener(ChatListener liste) {
+        listeners.add(liste);
+    }
+
+    public void unRegisterEchoListener(ChatListener liste) {
+        listeners.remove(liste);
+    }
+
+    private void notifyListeners(String msg) {
+        for (ChatListener listener : listeners) {
+            listener.messageReceived(msg);
+        }
+    }
+
+    public void connect(String address, int port) throws UnknownHostException, IOException {
+        this.port = port;
+        serverAddress = InetAddress.getByName(address);
+        socket = new Socket(serverAddress, port);
+        input = new Scanner(socket.getInputStream());
+        output = new PrintWriter(socket.getOutputStream(), true);  //Set to true, to get auto flush behaviour      
+        start();
+    }
+
+    public void send(String msg) {
+        output.println(msg);
+    }
+
+
 }
