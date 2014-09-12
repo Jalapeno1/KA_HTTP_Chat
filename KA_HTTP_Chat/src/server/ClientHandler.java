6fad2379cef87cdaeb24a2adab2b22bc39e60ceb
Phell0x <kaihultmann@10.50.137.94>
2014-09-12 11:37:15 +0200

    ClientHandler + ChatServer: Message recieved and handled - PRØVE2

--- KA_HTTP_Chat/src/server/ClientHandler.java
+++ KA_HTTP_Chat/src/server/ClientHandler.java
@@ -33,7 +33,7 @@ public class ClientHandler extends Thread {
                 server.addNewClient(protocolParts[1], this);
             }
             if (protocolParts[0].equals("SEND")) {
-                server.addMessage(protocolParts[1], this);
+                server.addMessage(protocolParts[2], this);
             }
             if (protocolParts[0].equals("CLOSE")) {
                 server.removeClient(protocolParts[1]);
